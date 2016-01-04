448ebed62993803ece44db6683467405f493056a
wangweishi 王威士 <wangweishi@pinganfang.com>
2016-01-04 12:01:49 +0800

    master:init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>phper - php开发者交流平台</title>
+        <meta charset="utf-8">
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 80px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">Welcome to phper live</div>
+				<div><img src="http://7xpqss.com1.z0.glb.clouddn.com/sleep.gif" /></div>
+            </div>
+        </div>
+    </body>
+</html>
